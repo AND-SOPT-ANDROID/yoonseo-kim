7849fc940707d37e3357af2c8a16cdd9b81f56bb
yskim6772 <yskim6772@gmail.com>
2024-10-11 08:50:19 +0900

    #1 [feat] : sign in activity ui

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -1,16 +1,14 @@
 package org.sopt.and
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.and.signin.SignInActivity
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ANDANDROIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val context = LocalContext.current
+                Intent(context, SignInActivity::class.java).apply {
+                    context.startActivity(this)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ANDANDROIDTheme {
-        Greeting("Android")
-    }
+fun MainPreview() {
+    ANDANDROIDTheme {}
 }
---
+++ app/src/main/java/org/sopt/and/signin/SignInActivity.kt
@@ -0,0 +1,197 @@
+package org.sopt.and.signin
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.R
+import org.sopt.and.component.EmailTextField
+import org.sopt.and.component.PasswordTextField
+import org.sopt.and.component.SignInTopBar
+import org.sopt.and.mypage.MyActivity
+import org.sopt.and.signup.SignUpActivity
+import org.sopt.and.ui.theme.ANDANDROIDTheme
+
+class SignInActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANDANDROIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SignIn(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SignIn(name: String, modifier: Modifier = Modifier) {
+    var id by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF1B1B1B)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        SignInTopBar("Wavve")
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        EmailTextField(id = id, onValueChange = { id = it }, hint = "이메일 주소 또는 아이디")
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        PasswordTextField(password = password, onValueChange = { password = it }, hint = "비밀번호")
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = {
+                Intent(context, MyActivity::class.java).apply {
+                    context.startActivity(this)
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                Color(0xFF1253FA)
+            )
+        ) {
+            Text("로그인")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "아이디 찾기",
+                fontSize = 12.sp,
+                color = Color(0xFFA3A3A3),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp),
+            )
+
+            Text(
+                text = "|",
+                fontSize = 12.sp,
+                color = Color(0xFFA3A3A3),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp),
+            )
+
+            Text(
+                text = "비밀번호 재설정",
+                fontSize = 12.sp,
+                color = Color(0xFFA3A3A3),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp),
+            )
+
+            Text(
+                text = "|",
+                fontSize = 12.sp,
+                color = Color(0xFFA3A3A3),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp),
+            )
+
+            Text(
+                text = "회원가입",
+                fontSize = 12.sp,
+                color = Color(0xFFA3A3A3),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp)
+                    .clickable {
+                        navigateToSignUp(context)
+                    },
+            )
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "또는 다른 서비스 계정으로 로그인",
+            fontSize = 14.sp,
+            color = Color(0xFFA3A3A3),
+            modifier = Modifier
+                .padding(horizontal = 15.dp),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo_group),
+            contentDescription = "logo images",
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "- SNS계정으로 간편하게 가입하여 서비스를 이용하실 수 있습니다.\n 기존 POOQ 계정 또는 Wavve 계정과는 연동되지 않으니 이용에 참고하세요.",
+            fontSize = 12.sp,
+            color = Color(0xFFA3A3A3),
+            modifier = Modifier
+                .padding(horizontal = 15.dp),
+        )
+    }
+}
+
+fun navigateToSignUp(context: Context) {
+    val intent = Intent(context, SignUpActivity::class.java)
+    context.startActivity(intent)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInPreview() {
+    ANDANDROIDTheme {
+        SignIn("Android")
+    }
+}
