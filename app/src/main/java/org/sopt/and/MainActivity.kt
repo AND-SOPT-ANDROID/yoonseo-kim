73378847d3321c1f381488ed658bd37c7ef71514
yskim6772 <yskim6772@gmail.com>
2024-10-25 12:22:19 +0900

    #3 [feat] : jetpack navigation

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -1,25 +1,84 @@
 package org.sopt.and
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import org.sopt.and.signin.SignInActivity
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.sopt.and.feature.nav.BottomNavigation
+import org.sopt.and.feature.signin.SignInScreen
+import org.sopt.and.feature.signup.SignUpScreen
+import org.sopt.and.model.BottomNavItem
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        initSignInActivity()
         setContent {
-            ANDANDROIDTheme {}
+            ANDANDROIDTheme {
+                val navController = rememberNavController()
+                MainScreen(navController)
+            }
         }
     }
+}
 
-    private fun initSignInActivity() {
-        val intent = Intent(this, SignInActivity::class.java)
-        startActivity(intent)
+@Composable
+fun MainScreen(navController: NavHostController) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    var registeredEmail by remember { mutableStateOf("") }
+    var registeredPassword by remember { mutableStateOf("") }
+
+    NavHost(navController = navController, startDestination = "signIn") {
+        composable("signIn") {
+            SignInScreen(
+                navController = navController,
+                registeredEmail = registeredEmail,
+                registeredPassword = registeredPassword,
+                snackbarHostState = snackbarHostState,
+                onSignUpClick = {
+                    navController.navigate("signUp")
+                },
+                onSignInSuccess = { email ->
+                    registeredEmail = email
+                    navController.navigate(BottomNavItem.Home.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                },
+                modifier = Modifier
+            )
+        }
+        composable("signUp") {
+            SignUpScreen(
+                navController,
+                modifier = Modifier,
+                onSignUpSuccess = { email, password ->
+                    registeredEmail = email
+                    registeredPassword = password
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable("home") {
+            BottomNavigation(
+                registeredEmail,
+                modifier = Modifier
+            )
+        }
     }
 }
