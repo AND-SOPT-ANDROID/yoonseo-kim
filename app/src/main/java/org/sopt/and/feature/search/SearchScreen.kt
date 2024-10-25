67de2bdedac9aa7830e04c84a4685f74cbadcb31
yskim6772 <yskim6772@gmail.com>
2024-10-25 17:56:09 +0900

    #3 [feat] : add search screen title

--- app/src/main/java/org/sopt/and/feature/search/SearchScreen.kt
+++ app/src/main/java/org/sopt/and/feature/search/SearchScreen.kt
@@ -1,9 +1,33 @@
 package org.sopt.and.feature.search
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.R
 
 @Composable
 fun SearchScreen() {
-    Text("Search Screen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.background_gray)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Search Screen",
+            fontSize = 20.sp,
+            color = colorResource(R.color.text_default_gray),
+            modifier = Modifier.padding(horizontal = 5.dp)
+        )
+    }
 }
