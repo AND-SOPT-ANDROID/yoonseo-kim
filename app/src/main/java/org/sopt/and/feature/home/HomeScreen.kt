372e5e5e00b9b0e93a709a67d9b1c5d42b079398
yskim6772 <yskim6772@gmail.com>
2024-10-25 12:21:11 +0900

    #3 [feat] : Single Activity Architecture

---
+++ app/src/main/java/org/sopt/and/feature/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package org.sopt.and.feature.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Home Screen")
+}
---
+++ app/src/main/java/org/sopt/and/feature/search/SearchScreen.kt
@@ -0,0 +1,9 @@
+package org.sopt.and.feature.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen() {
+    Text("Search Screen")
+}
