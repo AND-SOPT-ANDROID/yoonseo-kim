f28a36169fa0e2219f8fed878ee6c35e2899608a
yskim6772 <yskim6772@gmail.com>
2024-12-06 11:19:24 +0900

    #7 [feat] : clean architecture, hilt - sign in, get hobby

--- app/src/main/java/org/sopt/and/data/datasourceimpl/AuthDataSourceImpl.kt
+++ app/src/main/java/org/sopt/and/data/datasourceimpl/AuthDataSourceImpl.kt
@@ -1,8 +1,11 @@
 package org.sopt.and.data.datasourceimpl
 
 import org.sopt.and.data.datasource.AuthDataSource
+import org.sopt.and.data.dto.request.RequestSignInDto
 import org.sopt.and.data.dto.request.RequestSignUpDto
 import org.sopt.and.data.dto.response.BaseResponse
+import org.sopt.and.data.dto.response.ResponseHobbyDto
+import org.sopt.and.data.dto.response.ResponseSignInDto
 import org.sopt.and.data.dto.response.ResponseSignUpDto
 import org.sopt.and.data.service.AuthService
 import javax.inject.Inject
@@ -12,4 +15,10 @@ class AuthDataSourceImpl @Inject constructor(
 ) : AuthDataSource {
     override suspend fun signUp(request: RequestSignUpDto): BaseResponse<ResponseSignUpDto> =
         authService.signUp(request)
+
+    override suspend fun signIn(request: RequestSignInDto): BaseResponse<ResponseSignInDto> =
+        authService.signIn(request)
+
+    override suspend fun getHobby(request: String): BaseResponse<ResponseHobbyDto> =
+        authService.getHobby(request)
 }
--- app/src/main/java/org/sopt/and/data/repositoryimpl/AuthRepositoryImpl.kt
+++ app/src/main/java/org/sopt/and/data/repositoryimpl/AuthRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package org.sopt.and.data.repositoryimpl
 
 import org.sopt.and.data.datasource.AuthDataSource
+import org.sopt.and.data.mapper.toSignInRequest
 import org.sopt.and.data.mapper.toSignUpRequest
+import org.sopt.and.domain.entity.SignInModel
 import org.sopt.and.domain.entity.SignUpModel
 import org.sopt.and.domain.repository.AuthRepository
 import javax.inject.Inject
@@ -13,5 +15,17 @@ class AuthRepositoryImpl @Inject constructor(
         runCatching {
             val response = authDataSource.signUp(request.toSignUpRequest())
             response.result.userNumber
+        }
+
+    override suspend fun signIn(request: SignInModel): Result<String> =
+        runCatching {
+            val response = authDataSource.signIn(request.toSignInRequest())
+            response.result.token
+        }
+
+    override suspend fun getHobby(request: String): Result<String> =
+        runCatching {
+            val response = authDataSource.getHobby(request)
+            response.result.hobby
         }
 }
