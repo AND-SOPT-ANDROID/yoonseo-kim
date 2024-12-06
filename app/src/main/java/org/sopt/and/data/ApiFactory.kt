78d9996b99d6dd4ec52f38d4713d86c7b58dc602
yskim6772 <yskim6772@gmail.com>
2024-12-06 11:09:29 +0900

    #7 [refactor] : edit foldering

---
+++ app/src/main/java/org/sopt/and/data/ApiFactory.kt
@@ -0,0 +1,36 @@
+package org.sopt.and.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.and.BuildConfig
+import org.sopt.and.data.service.AuthService
+import retrofit2.Retrofit
+
+object ApiFactory {
+    private const val BASE_URL: String = BuildConfig.BASE_URL
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create(T::class.java)
+}
+
+object ServicePool {
+    val authService = ApiFactory.create<AuthService>()
+}
--- app/src/main/java/org/sopt/and/utils/ContextExt.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.and.utils
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
